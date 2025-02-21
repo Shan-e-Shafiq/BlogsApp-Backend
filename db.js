5eb4680f5cf9e696f1e48e341da794a371fffb52
Shan-e-Shafiq <shafiqshane6@gmail.com>
2025-02-21 23:48:33 +0500

    testing changes

--- db.js
+++ db.js
@@ -5,7 +5,11 @@ require('dotenv').config()
 async function ConnectToDB() {
     try {
         // console.log(process.env.MONGODB_URI)
-        await mongoose.connect(process.env.MONGODB_URI)
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000, // Timeout DB if too slow
+        })
         console.log("Connected....")
     } catch (error) {
         console.log(error)
