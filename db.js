97feea30b0c22c2c4717ae31c5d29f1b62d6c3d1
Shan-e-Shafiq <shafiqshane6@gmail.com>
2025-02-21 22:47:04 +0500

    testing changes

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 
 async function ConnectToDB() {
     try {
-        console.log(process.env.MONGODB_URI)
+        // console.log(process.env.MONGODB_URI)
         await mongoose.connect(process.env.MONGODB_URI)
         console.log("Connected....")
     } catch (error) {
